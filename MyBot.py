d24840ec19c3c085b47289b99daa5de2d7cbe246
Nicola Dainese <nicola.dainese96@gmail.com>
2018-10-20 15:07:38 +0200

    First commit

---
+++ MyBot.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#!C:\Users\Utente\Anaconda3 python
+# Import the Halite SDK, which will let you interact with the game.
+import hlt
+from hlt import constants
+
+import random
+import logging
+
+
+# This game object contains the initial game state.
+game = hlt.Game()
+def spawn_ship_cond(me, game, game_map):
+    if me.halite_amount >= constants.SHIP_COST and game.turn_number <= 1 and not game_map[me.shipyard].is_occupied:
+        return True
+    else:
+        return False
+    
+ship_status = {}
+# Respond with your name.
+game.ready("ND96_bot")
+
+while True:
+    # Get the latest game state.
+    game.update_frame()
+    # You extract player metadata and the updated map metadata here for convenience.
+    me = game.me
+    game_map = game.game_map
+
+    # A command queue holds all the commands you will run this turn.
+    command_queue = []
+    
+        
+    #movement block    
+    for ship in me.get_ships():
+        logging.info("Ship {} has {} halite.".format(ship.id, ship.halite_amount))
+        if ship.id not in ship_status:
+            ship_status[ship.id] = "exploring"
+        
+        if ship_status[ship.id] == "returning":
+            if ship.position == me.shipyard.position:
+                ship_status[ship.id] = "exploring"
+            else:
+                move = game_map.naive_navigate(ship, me.shipyard.position)
+                command_queue.append(ship.move(move))
+                continue
+        elif ship.halite_amount >= constants.MAX_HALITE / 4:
+            ship_status[ship.id] = "returning"
+            
+        # For each of your ships, move randomly if the ship is on a low halite location or the ship is full.
+        #   Else, collect halite.
+        if game_map[ship.position].halite_amount < constants.MAX_HALITE / 10 or ship.is_full:
+            command_queue.append(
+                ship.move(random.choice(["n", "s", "e", "w"])))
+                #ship.move('w'))
+        else:
+            command_queue.append(ship.stay_still())
+       
+
+    # If you're on the first turn and have enough halite, spawn a ship.
+    # Don't spawn a ship if you currently have a ship at port, though.
+    if spawn_ship_cond(me, game, game_map):
+        command_queue.append(game.me.shipyard.spawn())
+
+    # Send your moves back to the game environment, ending this turn.
+    game.end_turn(command_queue)
+
+
+
+    
+#def spawn_drop_cond(me, game, game_map, ship):
+    
